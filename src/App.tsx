8109c5f7075eabb51e56d0092a377fe3ca45c573
ZeanIkLaurie <luise.zapata@outlook.com>
2023-03-11 18:35:00 -0600

    this is the part were the router works

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import { Container } from "react-bootstrap"
 import { Routes, Route } from "react-router-dom"
+import { Home } from "../tabs/home"
+import { Checkout } from "../tabs/checkout"
+
 
 function App() {
   return <Container>
     <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/checkout" element={<Checkout/>}/>
     </Routes>
   </Container>
 }
