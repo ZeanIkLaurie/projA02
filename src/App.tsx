e485a4dca4eeabbabcc1982708e16740bebeea90
ZeanIkLaurie <luise.zapata@outlook.com>
2023-03-12 13:25:41 -0600

    router base

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
-import { Container } from "react-bootstrap"
-import { Routes, Route } from "react-router-dom"
-import { Home } from "../tabs/home"
-import { Checkout } from "../tabs/checkout"
-
+import { Container } from 'react-bootstrap'
+import { Routes, Route } from 'react-router-dom'
+import { Home } from '../tabs/home'
+import { Checkout } from '../tabs/checkout'
+import { Navbar } from '../components/navbar'
 
 function App() {
-  return <Container>
-    <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/checkout" element={<Checkout/>}/>
-    </Routes>
-  </Container>
+  return (
+  <>
+    <Navbar/>
+    <Container>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/checkout' element={<Checkout/>}/>
+      </Routes>
+    </Container>
+  </>
+  )
 }
 
 export default App
--- tabs/home.tsx
+++ tabs/home.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export function Home() {
     return <h1>select items</h1>
   }
