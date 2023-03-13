b14814b159f8c8dd313eb4a559b9d12d11516c2f
ZeanIkLaurie <luise.zapata@outlook.com>
2023-03-13 10:41:10 -0600

    .

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -5,7 +5,7 @@ import { MenuItem } from "../components/menuItem"
 export function Home() {
   return (
     <>
-      <h1>Store</h1>
+      <h1>Menu</h1>
       <Row md={2} xs={1} lg={3} className="g-3">
         {ITEMS.map(item => (
           <Col key={item.id}>
