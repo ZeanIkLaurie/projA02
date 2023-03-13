c5e381a27cb45006566c1dc59b8698e5852037e2
ZeanIkLaurie <luise.zapata@outlook.com>
2023-03-13 10:40:53 -0600

    rendering js crashed app xd

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,18 @@
-import * as ITEMS from '../data/items.json'
 import { Col, Row } from 'react-bootstrap'
-
+import * as ITEMS from '../data/items.json'
+import { MenuItem } from "../components/menuItem"
 
 export function Home() {
-  return <h1>Menu</h1>
+  return (
+    <>
+      <h1>Store</h1>
+      <Row md={2} xs={1} lg={3} className="g-3">
+        {ITEMS.map(item => (
+          <Col key={item.id}>
+            <MenuItem {...item} />
+          </Col>
+        ))}
+      </Row>
+    </>
+  )
 }
